dc982e4d8f327d426ffedbb4968a38d786547c53
Piotr Zalewski <bardzoglodnypiotrek@gmail.com>
2024-11-02 16:08:34 +0100

    feat: online-aware very realistic attract mode (99% accurate)

--- Themes/DDR A3/BGAnimations/ScreenCompany background/default.lua
+++ Themes/DDR A3/BGAnimations/ScreenCompany background/default.lua
@@ -39,4 +39,39 @@ return Def.ActorFrame {
 			s:sleep(10):linear(0.5):diffusealpha(1):sleep(2):linear(0.5):diffusealpha(0)
 		end,
 	};
+	Def.Sprite{
+		Texture="license1",
+		InitCommand=function(s) s:Center():diffusealpha(0):zoom(0.667) end,
+		OnCommand=function(s)
+			s:sleep(13):linear(0.25):diffusealpha(1):sleep(1):linear(0.25):diffusealpha(0)
+		end,
+	};
+	Def.Sprite{
+		Texture="license2",
+		InitCommand=function(s) s:Center():diffusealpha(0):zoom(0.667) end,
+		OnCommand=function(s)
+			s:sleep(14.5):linear(0.25):diffusealpha(1):sleep(1):linear(0.25):diffusealpha(0)
+		end,
+	};
+	Def.Sprite{
+		Texture="license3",
+		InitCommand=function(s) s:Center():diffusealpha(0):zoom(0.667) end,
+		OnCommand=function(s)
+			s:sleep(16):linear(0.25):diffusealpha(1):sleep(1):linear(0.25):diffusealpha(0)
+		end,
+	};
+	Def.Sprite{
+		Texture="license4",
+		InitCommand=function(s) s:Center():diffusealpha(0):zoom(0.667) end,
+		OnCommand=function(s)
+			s:sleep(17.5):linear(0.25):diffusealpha(1):sleep(1):linear(0.25):diffusealpha(0)
+		end,
+	};
+	Def.Sprite{
+		Texture="license5",
+		InitCommand=function(s) s:Center():diffusealpha(0):zoom(0.667) end,
+		OnCommand=function(s)
+			s:sleep(19):linear(0.25):diffusealpha(1):sleep(1):linear(0.25):diffusealpha(0)
+		end,
+	};
 };
