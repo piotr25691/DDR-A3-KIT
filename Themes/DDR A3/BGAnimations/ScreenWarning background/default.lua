66e9095a46c25f73f309689985f0487c337bd92a
luna <71488561+piotr25691@users.noreply.github.com>
2024-11-04 12:12:23 +0100

    fix: fade out on warning (part of attract mode)

--- Themes/DDR A3/BGAnimations/ScreenWarning background/default.lua
+++ Themes/DDR A3/BGAnimations/ScreenWarning background/default.lua
@@ -1,4 +1,8 @@
 return Def.ActorFrame {
+	Def.Quad{
+		InitCommand=function(s) s:diffuse(color("#ffffff")):FullScreen():diffusealpha(1) end,
+		OnCommand=function(s) s:sleep(0.01):linear(1):diffusealpha(0) end,
+	};
 	LoadActor(Model().."jp_warning")..{
 		InitCommand=function(s) s:FullScreen():diffusealpha(0) end,
 		OnCommand=function(s) s:sleep(1):linear(0.25):diffusealpha(1):sleep(3):linear(0.25):diffusealpha(0) end,
