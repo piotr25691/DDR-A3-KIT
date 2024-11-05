3f421385cb37d084779d0e53e7de0d9444e75496
luna <71488561+piotr25691@users.noreply.github.com>
2024-11-05 20:17:45 +0100

    fix: cleaner code for date code

--- Themes/DDR A3/BGAnimations/ModDate.lua
+++ Themes/DDR A3/BGAnimations/ModDate.lua
@@ -2,14 +2,24 @@ local identifier = "MDX"
 local region = "J"
 local type = "A"
 local spec = "A"
-local date = "20240522"
-local revision = "00"
+local date = 20240522
+local revision = 0
 
 return Def.BitmapText{
     Font="_futura pt medium 30px";
     InitCommand=function(s)  
-		--         MDX:A:A:A:Year/Month/Day/00
-		s:settext(identifier..":"..region..":"..type..":"..spec..":"..date..revision)
+		-- MDX:A:A:A:Year/Month/Day/00
+		s:settext(
+		    string.format(
+		       "%s:%s:%s:%s:%d%02d", 
+		       identifier,
+		       region,
+                       type,
+		       spec,
+		       date,
+		       revision
+		    )
+		)
 		s:xy(SCREEN_LEFT+64.2,SCREEN_TOP+8.5)
 		s:zoomx(0.42)
 		s:zoomy(0.4)
