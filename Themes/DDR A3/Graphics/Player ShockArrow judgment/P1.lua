4cad88fad7ba06aaeb0924fa3e58d695c5b0a7ea
Piotr Zalewski <bardzoglodnypiotrek@gmail.com>
2024-11-26 19:13:13 +0100

    refactor: slightly less buggy shock arrow judgments

--- Themes/DDR A3/Graphics/Player ShockArrow judgment/P1.lua
+++ Themes/DDR A3/Graphics/Player ShockArrow judgment/P1.lua
@@ -1,36 +1,64 @@
+-- NOTE: this implementation is buggy, don't rely on it.
+
+local currentEvent = nil
+local previousEvent = nil
+local isAnimating = false
+
 return Def.ActorFrame {
-	LoadActor(THEME:GetPathG("","HoldJudgment label 1x2/Hold"))..{
-		InitCommand=function(s) s:diffusealpha(0):animate(false):xy(SCREEN_LEFT+185, SCREEN_CENTER_Y-75) end,
-		OnCommand=function(s)
-			-- Fix position when playing Double Style
-			if GAMESTATE:GetCurrentStyle():GetName() == "double" then
-				s:xy(SCREEN_CENTER_X, SCREEN_CENTER_Y-75)
-			end
-		end,
-		JudgmentMessageCommand=function(self, params)
-			if params.Player ~= PLAYER_1 then return end;
-			if params.TapNoteScore == 'TapNoteScore_AvoidMine' 
-				or params.TapNoteScore == 'TapNoteScore_HitMine'
-			then
+    LoadActor(THEME:GetPathG("","HoldJudgment label 1x2/Hold"))..{
+        InitCommand=function(s) s:diffusealpha(0):animate(false):xy(SCREEN_LEFT+185, SCREEN_CENTER_Y-75) end,
+        OnCommand=function(s)
+            -- Fix position when playing Double Style
+            if GAMESTATE:GetCurrentStyle():GetName() == "double" then
+                s:xy(SCREEN_CENTER_X, SCREEN_CENTER_Y-75)
+            end
+        end,
+        JudgmentMessageCommand=function(self, params)
+            if params.Player ~= PLAYER_1 then return end;
+            if params.TapNoteScore == 'TapNoteScore_AvoidMine' 
+                or params.TapNoteScore == 'TapNoteScore_HitMine'
+            then
 				self:visible(true)
-				self:finishtweening()
-				
-				if params.TapNoteScore == 'TapNoteScore_HitMine' then
-					self:setstate(1)
-					self:queuecommand("Animate")
-				elseif params.TapNoteScore == 'TapNoteScore_AvoidMine' then
-					self:setstate(0)
-					self:queuecommand("Animate");
-				end
+
+                -- Prioritize HitMine over AvoidMine
+                if params.TapNoteScore == 'TapNoteScore_HitMine' then
+                    currentEvent = 'TapNoteScore_HitMine'
+                elseif currentEvent ~= 'TapNoteScore_HitMine' then
+                    currentEvent = params.TapNoteScore
+                end
+
+                -- Only start the animation if not already animating
+                if not isAnimating and params.TapNoteScore then
+                    self:queuecommand("Animate")
+                end
+            else
+                self:visible(false)
+            end
+        end;
+        AnimateCommand=function(self)
+            isAnimating = true
+            self:finishtweening()
+
+			-- NG
+            if currentEvent == 'TapNoteScore_HitMine' then
+                self:setstate(1)
+			-- OK
+            elseif currentEvent == 'TapNoteScore_AvoidMine' and previousEvent ~= 'TapNoteScore_HitMine' then
+                self:setstate(0)
+			-- OK after NG (buggy with only comparing 1 recent judgment)
 			else
-				self:visible(false)
-			end
-		end;
-		AnimateCommand=function(self)
-			self:diffusealpha(1)
-			self:zoom(0.28*1.5)
-			self:sleep(0.5)
-			self:diffusealpha(0)
-		end,
-	}
+				self:setstate(0)
+            end
+
+            self:diffusealpha(1)
+            self:zoom(0.28*1.5)
+            self:sleep(0.5)
+            self:diffusealpha(0)
+
+            -- Reset currentEvent after animation
+			previousEvent = currentEvent
+            currentEvent = nil
+            isAnimating = false
+        end,
+    }
 }
